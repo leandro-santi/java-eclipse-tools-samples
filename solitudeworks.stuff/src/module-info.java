d4fa8929133649f83de6287634181e127ccac7ab
Leandro Santiago <leandro.s4nt1@gmail.com>
2024-02-23 10:07:16 -0400

    Added rcp samples

---
+++ solitudeworks.stuff/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module solitudeworks.stuff {
+}
---
+++ solitudeworks.stuff/src/solitudeworks/stuff/tests/Test.java
@@ -0,0 +1,11 @@
+package solitudeworks.stuff.tests;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+
+	}
+
+}
