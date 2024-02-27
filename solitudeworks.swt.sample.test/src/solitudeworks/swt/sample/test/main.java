b8bbc4176c4a60b093fb4283012c90294e3990c3
Leandro Santiago <leandro.s4nt1@gmail.com>
2024-02-27 08:51:26 -0400

    Added initial swt example

---
+++ solitudeworks.swt.sample.test/src/solitudeworks/swt/sample/test/main.java
@@ -0,0 +1,66 @@
+package solitudeworks.swt.sample.test;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+
+public class main {
+
+	protected Shell shell;
+	private Text txtAline;
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			main window = new main();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shell = new Shell();
+		shell.setSize(450, 300);
+		shell.setText("SWT Application");
+		
+		txtAline = new Text(shell, SWT.BORDER | SWT.CENTER);
+		txtAline.setText("Test!");
+		txtAline.setBounds(180, 82, 83, 30);
+		
+		Button btnNewButton = new Button(shell, SWT.NONE);
+		btnNewButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(131, 147, 180, 67);
+		btnNewButton.setText("It's is a test!");
+
+	}
+}
